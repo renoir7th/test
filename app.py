be67b4d5e231c40bf2ec8e2fdac9ce55bd3ddf8e
renoir7th <renoir7th@hotmail.com>
2025-06-21 12:07:47 +0900

    Allow port configuration via ENV

--- app.py
+++ app.py
@@ -51,4 +51,5 @@ def process_image():
     return jsonify({"image": encoded})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
